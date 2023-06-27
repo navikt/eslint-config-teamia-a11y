fbddc9eb05b778a724457f4bd964b6faaeeaadd8
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2023-06-27 10:29:02 +0200

    Satte opp config med første rule.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# eslint-plugin-teamia-a11y
+# eslint-config-teamia-a11y
+
+Setup av eslint-config-jsx-a11y med regler vi ønsker å bruke likt i alle våre prosjekter.
+
+# Komme i gang
+
+Hvordan bygge, teste og kjøre koden viss aktuelt.
+
+---
+
+# Henvendelser
+
+Spørsmål knyttet til koden eller prosjektet kan stilles som issues her på GitHub
+
+## For NAV-ansatte
+
+Interne henvendelser kan sendes via Slack i kanalen [#teamia-utvikling](https://nav-it.slack.com/archives/C016KJA7CFK).
---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ["plugin:jsx-a11y/recommended"],
+  plugins: ["jsx-a11y"],
+  rules: {
+    "jsx-a11y/anchor-ambiguous-text": [
+      2,
+      {
+        words: [
+          "her",
+          "klikk",
+          "klikk her",
+          "lenken",
+          "linken",
+          "lenka",
+          "lenken her",
+          "linken her",
+          "lenka her",
+          "denne lenken",
+          "denne linken",
+          "denne lenka",
+        ],
+      },
+    ],
+  },
+  overrides: [],
+  settings: {},
+};
